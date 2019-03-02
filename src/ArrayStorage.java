3cfb554c498d6eb31a7e2f608a412fde17975b38
ElenaSheva <peyjm@yandex.ru>
2019-03-02 22:21:27 +0100

    HW2: Realisation ArrayStorage: add update(Resume r): after review N 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,75 +2,81 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10_000];
-    int sizeOfStorage = 0;
+    private  Resume[] storage = new Resume[10_000];
+    private  int sizeOfStorage = 0;
 
-    void clear() {
-        for (int lCount = 0; lCount < sizeOfStorage; lCount++){
-            storage[lCount] = null;
+    public void clear() {
+        for (int сount = 0; сount < sizeOfStorage; сount++) {
+            storage[сount] = null;
         }
         sizeOfStorage = 0;
     }
 
-    void update(Resume resumeToUpdate) {
-        int lIndex = getIndex(resumeToUpdate.uuid);
-        if (lIndex != -1) {
-            storage[lIndex] = resumeToUpdate;
-            System.out.println("Resume '" + resumeToUpdate.uuid + "' is updated!");
+    public void update(Resume resumeToUpdate) {
+        int index = getIndex(resumeToUpdate.getUuid());
+        if (index != -1) {
+            storage[index] = resumeToUpdate;
+        } else {
+            System.out.println("Resume '" + resumeToUpdate.getUuid() + "' not exist in Array!");
         }
     }
 
-    void save(Resume resumeToSave) {
-        if (sizeOfStorage == storage.length){
+    public void save(Resume resumeToSave) {
+        if (sizeOfStorage == storage.length) {
             System.out.println("Array is full!");
         } else {
-            if (getIndex(resumeToSave.uuid) == -1){
+            if (getIndex(resumeToSave.getUuid()) == -1) {
                 storage[sizeOfStorage] = resumeToSave;
                 sizeOfStorage++;
-                System.out.println("Resume is saved!");
+            } else {
+                System.out.println("Resume '" + resumeToSave.getUuid() + "' exist in Array!");
             }
         }
-
     }
 
-    Resume get(String uuid) {
-        int lIndex = getIndex(uuid);
-        return lIndex == -1 ? null : storage[lIndex];
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume '" + uuid + "' not exist in Array!");
+            return null;
+        } else {
+            return storage[index];
+        }
     }
 
-    void delete(String uuid) {
-        int lResumeIndex = getIndex(uuid);
-        if (lResumeIndex != -1){
-            storage[lResumeIndex] = storage[sizeOfStorage - 1];
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index != -1) {
+            storage[index] = storage[sizeOfStorage - 1];
             storage[sizeOfStorage - 1] = null;
             sizeOfStorage--;
+        } else {
+            System.out.println("Resume '" + uuid + "' not exist in Array!");
         }
     }
 
     private int getIndex(String uuid) {
-        if (uuid == null){
-            System.out.println("uuid is null!");
+        if (uuid == null) {
             return -1;
         }
-        for (int lCount = 0; lCount < sizeOfStorage; lCount++){
-            if (uuid.equals(storage[lCount].uuid)){
-                return lCount;
+        for (int сount = 0; сount < sizeOfStorage; сount++) {
+            if (uuid.equals(storage[сount].getUuid())) {
+                return сount;
             }
         }
-        System.out.println("Resume '" + uuid + "' not exist in Array!");
         return -1;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] temp = new Resume[sizeOfStorage];
         System.arraycopy(storage, 0, temp, 0, sizeOfStorage);
         return temp;
     }
 
-    int size() {
+    public int size() {
         return sizeOfStorage;
     }
 }
